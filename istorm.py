ca968e8f7c08bb8e2c3d86f6a7ac289304377cda
unknown <p.salome@bdx-135.serma.local>
2011-11-25 12:06:37 +0100

    Signed-off-by: Pascal SALOME <p.salome@serma.com>
    addition of leackage pick possibilities

--- istorm.py
+++ istorm.py
@@ -28,7 +28,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.interactive(True)
-from thunderstorm.interact import my_storm
+from thunderstorm.interact import mystorm
 
 def console(variables):
     import os, sys
--- thunderstorm/thunder/importers/util_serma.py
+++ thunderstorm/thunder/importers/util_serma.py
@@ -93,7 +93,7 @@ class ReadSERMA(object):
         for data_name in ('tlp', 'valim_tlp', 'tlp_pulses',
                           'valim_leak', 'leak_evol', 'offsets_t'):
             num_data[data_name] = np.array(self.data[data_name])
-        num_data['leak_data'] = self.data['leak_data']
+        num_data['leak_data'] = np.array(self.data['leak_data'])
         num_data['delta_t'] = self.data['delta_t']
         return num_data
 
